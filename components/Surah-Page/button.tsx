de48240d640b99eb0dcba9816fa3d46bdfef0e60
Framadhita <framadhita@outlook.com>
2023-09-08 13:39:58 +0700

    simplify tailwind class by changing to tailwind group

--- components/Surah-Page/button.tsx
+++ components/Surah-Page/button.tsx
@@ -1,8 +1,8 @@
 import { MouseEventHandler, ReactNode } from "react"
 
 export default function Button({ children, onClick = () => { } }: { children: ReactNode, onClick?: MouseEventHandler }) {
-  return <button onClick={onClick} className={`p-1 rounded-full flex items-center justify-center w-8 h-8 transition-all relative button hover:text-sec-color-light [&>div]:hover:bg-sec-color-light active:scale-90 rounded-full`}>
-    <div className="absolute opacity-30 w-full h-full transition-all rounded-full" />
+  return <button onClick={onClick} className={`p-1 flex items-center justify-center w-8 h-8 transition-all relative button hover:text-sec-color-light group active:scale-90 rounded-full`}>
+    <div className="absolute opacity-30 w-full h-full transition-all rounded-full group-hover:bg-sec-color-light" />
     {children}
   </button>
 }
