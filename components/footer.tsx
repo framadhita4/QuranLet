a69267d947947ee90a2c24971449c452588dcb23
Framadhita <framadhita@outlook.com>
2023-09-19 11:10:55 +0700

    edit styling for recommendations

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,38 @@
 import Link from "next/link";
 
+const recommendations = [
+  {
+    name: "Ayat Kursi",
+    href: "/2?verse=255"
+  },
+  {
+    name: "Surah Yasin",
+    href: "/36"
+  },
+  {
+    name: "Surah Al-Mulk",
+    href: "/67"
+  },
+  {
+    name: "Surah Ar-Rahman",
+    href: "/55"
+  },
+  {
+    name: "Surah Al-Waqi'ah",
+    href: "/56"
+  },
+  {
+    name: "Surah Al-Kahf",
+    href: "/18"
+  },
+  {
+    name: "Surah Al-Muzammil",
+    href: "/73"
+  }
+]
+
 export default function Footer() {
-  return <div className="text-sm sm:text-base p-5 m-auto text-gray-700 md:w-11/12 sm:flex sm:gap-32 mb-16 border-t-2">
+  return <div className="text-sm sm:text-base p-5 m-auto text-gray-700 md:w-11/12 sm:flex sm:gap-32 mb-12 border-t-2">
     <div className="flex-1 mb-10">
       <h1 className="text-xl font-semibold">Quranlet</h1>
       <h2 className="text-sm sm:text-base font-bold my-2">Al-Quran dengan terjamahan dan tafsir bahasa Indonesia</h2>
@@ -9,14 +40,10 @@ export default function Footer() {
     </div>
     <div className="flex-1">
       <h1 className="text-sm sm:text-base mb-2 font-semibold">Rekomendasi</h1>
-      <div className="flex flex-col">
-        <Link className="hover:underline w-fit" href="/2?verse=255">Ayat Kursi</Link>
-        <Link className="hover:underline w-fit" href="/36">Surah Yasin</Link>
-        <Link className="hover:underline w-fit" href="/67">Surah Al-Mulk</Link>
-        <Link className="hover:underline w-fit" href="/55">Surah Ar-Rahman</Link>
-        <Link className="hover:underline w-fit" href="/56">Surah Al-Waqi&apos;ah</Link>
-        <Link className="hover:underline w-fit" href="/18">Surah Al-Kahf</Link>
-        <Link className="hover:underline w-fit" href="/73">Surah Al-Muzammil</Link>
+      <div className="flex flex-wrap gap-2">
+        {
+          recommendations.map((e, i) => <Link className="p-2 border-[1.5px] rounded w-fit hover:text-sec-color-light hover:border-sec-color-light" key={i} href={e.href}>{e.name}</Link>)
+        }
       </div>
     </div>
   </div>
